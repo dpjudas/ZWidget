cc10824f5a2aaf374c3822af1457c11fe52246ab
LupertEverett <86886753+LupertEverett@users.noreply.github.com>
2025-11-06 01:21:02 +0300

    SDL2DisplayWindow: Use SDLNativeHandle

--- src/window/sdl2/sdl2_display_window.h
+++ src/window/sdl2/sdl2_display_window.h
@@ -2,7 +2,7 @@
 
 #include <unordered_map>
 #include <zwidget/window/window.h>
-#include <zwidget/window/sdl2nativehandle.h>
+#include <zwidget/window/sdlnativehandle.h>
 #include <SDL2/SDL.h>
 
 class SDL2DisplayWindow : public DisplayWindow
@@ -95,7 +95,7 @@ public:
 	static Uint32 ExecTimer(Uint32 interval, void* id);
 
 	DisplayWindowHost* WindowHost = nullptr;
-	SDL2NativeHandle Handle;
+	SDLNativeHandle Handle;
 	SDL_Renderer* RendererHandle = nullptr;
 	SDL_Texture* BackBufferTexture = nullptr;
 	int BackBufferWidth = 0;
